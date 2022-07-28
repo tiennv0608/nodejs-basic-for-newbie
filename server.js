8d207d3d6ee0be3f46ce14f1166105694aee9558
James Nguyen <nv.tien1@samsung.com>
2022-07-28 08:03:40 +0700

    done hello world

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  console.log("run request ...");
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h3>Hello world! </h3>");
-  res.end();
+app.get("/", (req, res) => {
+  res.send("Hello World! với tôi");
 });
 
-server.listen(3000, "localhost", () => {
-  console.log("Node.JS server is running on port: 3000");
+app.get("/about", (req, res) => {
+  res.send(`I'm Việt Tiến`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
