6d058c3e8b9d11820ca8551aa0b2651820c1397a
James Nguyen <nv.tien1@samsung.com>
2022-08-05 07:58:02 +0700

    done req params

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,16 +1,12 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 // create the connection to database
-const connection = mysql.createConnection({
+console.log("Creating connection pool...");
+
+const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "nodejsbasic",
 });
 
-// simple query
-// connection.query("SELECT * FROM `users` ", function (err, results, fields) {
-//   console.log(results);
-//   console.log(results[0]);
-// });
-
-export default connection;
+export default pool;
--- src/routers/web.js
+++ src/routers/web.js
@@ -5,6 +5,7 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
   router.get("/", homeController.getHomePage);
+  router.get("/detail/user/:userId", homeController.getDetailPage);
 
   router.get("/about", homeController.getAbout);
   return app.use("/", router);
